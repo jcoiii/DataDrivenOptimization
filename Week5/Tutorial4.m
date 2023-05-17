a6db75fe03abfd81ba05fcb90d94fec72c6f242a
Jonathan Chandra <chandrajonathan5@gmail.com>
2023-05-17 10:40:29 +0200

    Add files via upload

--- Week5/Tutorial4.m
+++ Week5/Tutorial4.m
@@ -1,3 +1,42 @@
+
+%% Pre coding Problem 1
+% GD and SGD differ in how they update the parameters:
+
+% GD computes the gradient of the cost function with respect to the 
+% parameters for the entire training dataset. It updates the parameters 
+% in the direction of the negative gradient to minimize the cost function. 
+% Basically it uses all of the data at each iteration.
+
+% SGD updates the parameters for each training example one at a time. 
+% it computes the gradient of the cost function and updates the parameters 
+% for each individual data point. 
+
+% Additional notes*:
+% SGD uses one data point at a time, the path to the minimum of the cost 
+% function can be noisier than the path taken by GD.
+
+% Read about Mini-Batch Gradient Descent*. <- comes in between GD and SGD 
+
+% So, the main difference of GD and SGD is how they use the data to compute 
+% the gradient and update the parameters. GD uses all the data at once, 
+% while SGD uses one data point at a time.
+
+%% Pre coding Problem 2
+% The update is performed for each training data i,
+
+%% Pre coding Problem 3
+% The convergence of the cost function of SGD is more noisy and less smooth
+% than in GD. This is because the parameter updates in SGD are based on 
+% individual training data i, which may lead to frequent changes in 
+% direction and magnitude of the updates.
+
+% SGD should converges faster than Batch Gradient Descent in terms of 
+% computation time, in the case of large datasets, 
+% because it makes updates with every data i as opposed to waiting to 
+% see the entire dataset.
+
+
+%%
 close all;
 clear;
 clc;
@@ -20,62 +59,60 @@ C = @(theta, x_i, y_i) 1/(2*length(y_i)) * (h(theta, x_i) - y_i)' ...
 * (h(theta, x_i) - y_i);
 gradC = @(theta, x_i, y_i) 1/length(y_i) * (x_i' * (h(theta, x_i) - y_i));
 
-% Stochastic gradient descent
+% SGD
 alpha = 0.001;  
 tol = 1e-4;  % convergence threshold
-max_iter = 1e5;  % maximum iterations to prevent infinite loop
+max_iter = 1e3;  % maximum iter to prevent infinite loop
 iter = 0;
 idx = 0;
-C_prev = inf;  % Initialize the previous cost to a large number
-C_iter_history = zeros(max_iter * length(y_i), 1);  % Cost for each iteration
-C_full_history = zeros(max_iter, 1);  % Cost for the full dataset
+C_prev = inf;  % define the previous cost to a large number
+C_iter_hist = zeros(max_iter * length(y_i), 1);  % each iteration cost
+C_full_hist = zeros(max_iter, 1);  % full dataset cost
 
 while true
-    % Randomly shuffle the dataset
+    % Try to randomize the x_i and y_i, 
     rand_indices = randperm(length(y_i));
     x_i = x_i(rand_indices, :);
     y_i = y_i(rand_indices);
     
     for i = 1:length(y_i)
-        % Update the parameters for each training example
         theta = theta - alpha * gradC(theta, x_i(i, :), y_i(i));
 
         C_iter = C(theta, x_i(i, :), y_i(i));
         idx = idx + 1;
-        C_iter_history(idx) = C_iter;
+        C_iter_hist(idx) = C_iter;
     end
     
-    % Compute and store the cost for the full dataset
+
     C_full = C(theta, x_i, y_i);
     iter = iter + 1;
-    C_full_history(iter) = C_full;
+    C_full_hist(iter) = C_full;
     disp(['Current iteration: ', num2str(iter)]);
-    % Check for convergence
+
     if abs(C_full - C_prev) < tol
         break;
     elseif iter >= max_iter
-        disp('Maximum iterations reached without convergence.')
+        disp('Maximum iterations achieved but no convergence happens.')
         break;
     end
     
-    % Store the current cost for the next iteration
     C_prev = C_full;
 end
 
-% Trim cost histories to the number of iterations actually performed
-C_iter_history = C_iter_history(1:idx);
-C_full_history = C_full_history(1:iter);
+% Trim to last iteration
+C_iter_hist = C_iter_hist(1:idx);
+C_full_hist = C_full_hist(1:iter);
 
-% Plot the cost for each iteration
+% Plot: (1) the cost of each iteration, (2) the cost of the full dataset
 figure;
-plot(C_iter_history);
+plot(C_iter_hist);
 title('Cost for Each Iteration');
 xlabel('Iteration');
 ylabel('Cost');
 
 % Plot the cost for the full dataset over iterations
 figure;
-plot(C_full_history);
+plot(C_full_hist);
 title('Cost for Full Dataset Over Iterations');
 xlabel('Iteration');
 ylabel('Cost');
